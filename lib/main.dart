e1a3e3f9cc8142da74f08bbc303f2efcd3dc50c7
Sofyan R <sofyan423@gmail.com>
2025-05-13 14:29:31 +0700

    chore: disable orientation landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:marknotes/core/common/entities/app_theme_entity.dart';
@@ -21,7 +22,10 @@ void main() async {
   );
   // Initialize dependencies
   await initDependencies();
-  runApp(const MarkNotesApp());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((value) => runApp(const MarkNotesApp()));
 }
 
 class MarkNotesApp extends StatelessWidget {
